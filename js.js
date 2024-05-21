0313530f0a0ab9342a01cd13a1d0b3fb6704ef1e
ZahraJou <zahrajoulaei@gmail.com>
2024-05-21 11:43:31 -0400

    first commit

---
+++ js.js
@@ -0,0 +1,21 @@
+let num = 44;
+if(num>=90){
+    console.log("A")
+}
+   else if(num>=80) {
+        console.log("A")
+    }
+   else if(num>=70) {
+        console.log("C")
+    }
+   else if(num>=55) {
+        console.log("D")
+    }
+  else  if(num<55) {
+        console.log("F")
+    }
+
+
+
+
+
